bd38341b12c17fd6f918c64b6629d9a7dd220a07
최현준 <choijjunii1@gmail.com>
2025-05-23 17:01:15 +0900

    Feat: PasswordField value,onChange Props 설정

--- client/src/components/signup/fields/PasswordField.tsx
+++ client/src/components/signup/fields/PasswordField.tsx
@@ -1,8 +1,19 @@
-import { Input } from "@/components/common/Input"
+import { useState } from "react";
+import { Input } from "@/components/common/Input";
+import { InputProps } from "@/components/common/Input";
 
-export function PasswordField() {
+// Input 인터페이스에 타입 추가
+interface PasswordFieldProps extends InputProps {
+  password: string;
+}
 
-  // 비밀번호,비밀번호확인 일치확인 로직 추가
+export function PasswordField({ password, onChange }: PasswordFieldProps) {
+  const [passwordCheck, setPasswordCheck] = useState("");
+
+  // 비밀번호 체크 확인
+  const handlePasswordCheck = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPasswordCheck(e.target.value);
+  };
 
   return (
     <div>
@@ -11,13 +22,17 @@ export function PasswordField() {
         label="비밀번호"
         placeholder="비밀번호"
         type="password"
+        value={password}
+        onChange={onChange}
       />
       <Input
         name="password-check"
         label="비밀번호확인"
         placeholder="비밀번호확인"
-        type="passowrd"
+        type="password"
+        value={passwordCheck}
+        onChange={handlePasswordCheck}
       />
     </div>
-  )
+  );
 }
