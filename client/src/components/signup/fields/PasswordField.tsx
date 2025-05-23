7e7eb43e987aaca8c0351dada07c4d3316585749
김민지 <twoji3019@naver.com>
2025-05-23 17:39:24 +0900

    Feat : 상태 관리 및 유효성 검사

--- client/src/components/signup/fields/PasswordField.tsx
+++ client/src/components/signup/fields/PasswordField.tsx
@@ -1,22 +1,39 @@
 import { Input } from "@/components/common/Input"
+import { useState } from "react"
 
-export function PasswordField() {
+export function PasswordField({setPassword}: {setPassword: React.Dispatch<React.SetStateAction<string>>}) {
+
+  // 비밀번호 상태 관리
+  const [pw, setPw] = useState('')
+  const [pwCheck, setPwCheck] = useState('')
 
   // 비밀번호,비밀번호확인 일치확인 로직 추가
 
+  // 영문 숫자 특수문자 포함 8글자 이상 50자 미만
+  const checkPw = (pw:string):boolean=>{
+    const isValid = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,50}$/.test(pw);
+
+    return isValid
+  }
+  
+  
   return (
     <div>
       <Input
         name="password"
         label="비밀번호"
         placeholder="비밀번호"
+        value={pw}
         type="password"
+        onChange={()=>{}}
       />
       <Input
         name="password-check"
         label="비밀번호확인"
         placeholder="비밀번호확인"
         type="passowrd"
+        value={pwCheck}
+        onChange={()=>{}}
       />
     </div>
   )
