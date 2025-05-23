581df4c8837d40fee34ef0e5e1aebda1bce933e5
전선일 <jeonsunil1225@gamil.com>
2025-05-23 18:06:02 +0900

    Feat: 로그인 기능 구현

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,10 +1,12 @@
+import { LoginService } from './login.service';
 import { Body, Controller, Post, Get,HttpCode } from "@nestjs/common";
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService,    private readonly loginService: LoginService) {}
   @Get()
   async getHello() {
     return 'Hello World!';
@@ -13,5 +15,10 @@ export class AuthController {
   @HttpCode(201)
   async register(@Body() dto: RegisterDto) {
     return this.authService.register(dto);
+  }
+  @Post('login')
+  @HttpCode(201)
+  async login(@Body() dto : LoginDto) {
+    return this.loginService.login(dto);
   }
 }
--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { DbModule } from '../database/db.module'; // 👈 이거 추가
+import { LoginService } from './login.service';
 
 @Module({
     imports: [DbModule],
     controllers: [AuthController],
-    providers: [AuthService],
+    providers: [AuthService, LoginService],
 })
 export class AuthModule {}
