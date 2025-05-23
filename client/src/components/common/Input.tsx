a65758ddaa4eb054ee46fd88fb85d76794e8af31
김민지 <muunji@github.com>
2025-05-23 10:54:54 +0900

    Test : 인터페이스 속성 변경, Input 작성

--- client/src/components/common/Input.tsx
+++ client/src/components/common/Input.tsx
@@ -2,13 +2,28 @@ import React from "react";
 
 // 인터페이스
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement>{
-  lable?:string;
-  palceholder?:string;
+  placeholder?:string;
   type?:string;
   name?:string;
+  value?:string;
+  className?:string;
 }
 
 // 부모컴포넌트에서 직접 접근
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({placeholder,type,name,value,className, ...props},ref)=>{
+    return (
+        <input
+          ref={ref}
+          type={type}
+          name={name}
+          placeholder={placeholder}
+          className={className}
+          {...props}
+        />
+    )
+  }
+)
 
 // displayName
 
