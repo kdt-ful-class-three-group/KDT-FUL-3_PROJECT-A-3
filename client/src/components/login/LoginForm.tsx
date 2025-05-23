2454d822aa5199e26f379f186994d9a119ecd7b8
김민지 <twoji3019@naver.com>
2025-05-23 11:54:14 +0900

    Feat : 렌더 작성

--- client/src/components/login/LoginForm.tsx
+++ client/src/components/login/LoginForm.tsx
@@ -1,9 +1,20 @@
 import { useState } from "react";
 import Input from "../common/Input";
+import { Button } from "../common/Button";
 
 export default function LoginForm() {
   // 아이디, 비밀번호 상태 관리
   const [id, setId] = useState("");
   const [email, setEmail] = useState("");
+
+  // 아이디, 비밀번호 변경 함수
   
+  return (
+    // 로그인 폼
+    <form onSubmit={}>
+      <Input />
+      <Input />
+      <Button type="submit">로그인</Button>
+    </form>
+  )
 }
