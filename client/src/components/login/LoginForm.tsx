cea7e5c98a3994b7184da6ca06fb21088d830fda
김민지 <twoji3019@naver.com>
2025-05-23 11:52:34 +0900

    Feat : 로그인폼 작성, 상태 관리

---
+++ client/src/components/login/LoginForm.tsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import Input from "../common/Input";
+
+export default function LoginForm() {
+  // 아이디, 비밀번호 상태 관리
+  const [id, setId] = useState("");
+  const [email, setEmail] = useState("");
+  
+}
