735a346c598e6472f92b28c895897dbc829cd557
JadenMeister <bigdragon00@naver.com>
2025-05-23 12:56:51 +0900

    Feat: [회원가입] app Module 구성 #8

---
+++ server/src/app.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { DbModule } from './database/db.module';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+    imports: [ConfigModule.forRoot({isGlobal:true}),AuthModule, DbModule],
+})
+export class AppModule {}
