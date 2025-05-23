bd9d7bcc09f5bde763386a798c0d66e244656e2b
김민지 <twoji3019@naver.com>
2025-05-23 11:45:51 +0900

    Feat : 로그인에 필요한 요소 작성

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -3,5 +3,13 @@ import { Button } from "@/components/common/Button";
 import Input from "@/components/common/Input";
 
 export default function LoginPage(){
-  
+  // 페이지 이름
+
+  // input 2개 - 아이디, 비밀번호
+
+  // 아이디찾기, 비밀번호 찾기, 회원가입 링크
+
+  // 로그인 버튼
+
+  // 간편로그인
 }
