e2e5567bd5bfe408be9e17a9f922cfba269b2b72
최현준 <choijjunii1@gmail.com>
2025-05-23 14:38:02 +0900

    Feat: 아이디 찾기 기본 폼 생성

---
+++ client/src/app/find/page.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { Input } from "@/components/common/Input";
+import { Button } from "@/components/common/Button";
+
+export function Find() {
+  return;
+  <div>
+    <form onSubmit={() => { }}>
+      <Input
+        label=""
+        name="생년월일"
+        placeholder="생년월일 8자리 YYYYMMDD"
+        value=""
+      />
+      <Input
+        label=""
+        name=""
+        value=""
+        />
+      <Button
+        name="확인"
+        type="submit"
+      />
+    </form>
+    <div>
+      
+    </div>
+  </div>;
+}
