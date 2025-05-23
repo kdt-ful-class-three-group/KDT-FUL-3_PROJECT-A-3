c4b2cef485436afc9738c25a9389df095de50d47
김민지 <muunji@github.com>
2025-05-23 11:03:29 +0900

    Test : classname 속성 삭제

--- client/src/components/common/Input.tsx
+++ client/src/components/common/Input.tsx
@@ -6,19 +6,17 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement>{
   type?:string;
   name?:string;
   value?:string;
-  className?:string;
 }
 
 // 부모컴포넌트에서 직접 접근
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({placeholder,type,name,value,className, ...props},ref)=>{
+  ({placeholder,type,name,value, ...props},ref)=>{
     return (
         <input
           ref={ref}
           type={type}
           name={name}
           placeholder={placeholder}
-          className={className}
           {...props}
         />
     )
