47cc4feab5464bf61922c756da53132abd9e1a19
김민지 <twoji3019@naver.com>
2025-05-26 16:41:51 +0900

    Feat : 로그인 엔드포인트로 요청 보냄

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -5,12 +5,24 @@ import AuthButton from "@/components/login/AuthButton";
 
 export default function LoginPage(){
 
+  // 8008에서 데이터 조회
+  const checkLogin = async(id:string, pw:string)=>{
+    const rest = await fetch('http://localhost:8008/auth/login',{
+      method:'POST',
+      headers:{'Content-Type':'application/json'},
+      body:JSON.stringify({id,pw})
+    })
+
+    const data = await rest.json()
+
+  }
+
   return(
     <div>
       {/* 페이지 이름 */}
       <h1>로그인</h1>
       {/* 로그인폼 */}
-      <LoginForm onLogin={(id, pw) => console.log(id, pw)}/>    
+      <LoginForm onLogin={checkLogin}/>    
       {/* 간편로그인 */}
       <div>
         <p>간편로그인_추가</p>
