563efd56edcb16814b4465df4e72c24d38503f1e
김민지 <muunji@github.com>
2025-05-26 12:40:16 +0900

    Test : store 생성

---
+++ client/src/store/index.ts
@@ -0,0 +1,13 @@
+// store 생성
+import { configureStore } from "@reduxjs/toolkit";
+import signupReducre from './signupSlice'
+
+export const store = configureStore({
+  reducer:{
+    signup:signupReducre
+  }
+})
+
+//타입 정의
+export type RootStatet = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
