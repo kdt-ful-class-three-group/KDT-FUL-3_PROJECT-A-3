fdd6b94cda6b9bdeb881037dace5990379403303
전선일 <jeonsunil1225@gamil.com>
2025-05-26 14:29:10 +0900

    Docs: users관련 ts들과 main.ts 에 대한 주석 추가

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,11 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
 
+// * Controller 데코레이터의 괄호 안에 경로를 지정하지 않으면, 기본적으로 해당 컨트롤러의 경로는 '/users'가 된다.
+// * 괄호안에 경로를 지정하면, 해당 경로가 컨트롤러의 기본 경로가 된다.
 @Controller()
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  // * @Get 데코레이터는 HTTP GET 요청을 처리하고, 괄호안에 경로를 지정하지 않으면, 기본적으로 컨트롤러의 기본 경로가 된다.
+  // * 따라서, 이 컨트롤러의 기본 경로는 '/users'가 되고, 이 메서드는 '/users' 경로로 GET 요청을 처리한다.
   @Get()
+  // * getAllUsers 메서드는 UsersService의 getAllUsers 메서드를 호출하여 모든 유저 정보를 가져온다.
   async getAllUsers() {
     return this.usersService.getAllUsers();
   }
