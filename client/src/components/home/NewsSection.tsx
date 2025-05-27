9c256484a8773f9c33f999c36efce7288f8e1570
김민지 <twoji3019@naver.com>
2025-05-27 17:37:40 +0900

    Test : news api 사용해서 브라우저 출력

--- client/src/components/home/NewsSection.tsx
+++ client/src/components/home/NewsSection.tsx
@@ -3,10 +3,25 @@ import { Section } from "../common/Section";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
+//뉴스 데이터 타입 지정
+type Article = {
+  source:{
+    id:string|null;
+    name:string;
+  };
+  author:string|null;
+  title:string;
+  description:string;
+  url:string;
+  urlToImage:string|Blob|undefined;
+  publishedAt:string;
+  content:string;
+}
+
 export function NewsSection() {
 
   //뉴스 상태
-  const [news, setNews] = useState([])
+  const [news, setNews] = useState<Article[]>([])
 
   const apiKey = process.env.NEXT_PUBLIC_NEWS_API_KEY
   // everythingUrl-한국기사 & 최신순
@@ -17,7 +32,10 @@ export function NewsSection() {
     async function fetchNews(){
       try{
         const res = await axios.get(url)
+        //디버깅
         console.log(res.data.articles)
+        //데이터 담기
+        setNews(res.data.articles)
       }
       catch(err){
         console.error('뉴스 불러오기 실패',err)
@@ -33,7 +51,20 @@ export function NewsSection() {
     <Section
       title="뉴스"
       description="최신 뉴스와 업데이트를 확인하세요."
-      children={<p>뉴스 섹션 내용이 여기에 들어갑니다.</p>}
+      children={
+        <div>
+        {news.length>0 ?(
+          <div>
+
+          <h3>{news[1].title}</h3>
+          <a href={news[1].url} target="_blank" title="news">
+          <img src={news[1].urlToImage} alt="" />
+          </a>
+          </div>
+        ) : <p>,,,</p>}
+        </div>
+        
+      }
     />
   )
 }
