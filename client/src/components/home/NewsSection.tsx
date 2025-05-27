5ca16f477cb8b27fbe2339d22c40598c75582d99
김민지 <twoji3019@naver.com>
2025-05-27 16:08:00 +0900

    Feat : 뉴스 데이터 가져옴

--- client/src/components/home/NewsSection.tsx
+++ client/src/components/home/NewsSection.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { Section } from "../common/Section";
 import { useEffect, useState } from "react";
 import axios from "axios";
@@ -8,11 +9,22 @@ export function NewsSection() {
   const [news, setNews] = useState([])
 
   const apiKey = process.env.NEXT_PUBLIC_NEWS_API_KEY
-  const url = `https://newsapi.org/v2/top-headlines?country=kr&apiKey=${apiKey}`
+  const url = `https://newsapi.org/v2/everything?q=ai&sortBy=publishedAt&apiKey=${apiKey}`;
 
   //axios
   useEffect(()=>{
+    async function fetchNews(){
+      try{
+        const res = await axios.get(url)
+        console.log(res.data)
+      }
+      catch(err){
+        console.error('뉴스 불러오기 실패',err)
+      }
+    }
 
+    //실행
+    fetchNews()
   },[])
 
 
