16b64c39fd5657bb201f4290bf419dee5ae0f299
전선일 <jeonsunil1225@gamil.com>
2025-05-27 17:24:50 +0900

    Feat: 로그인 서비스에서 jwt토큰을 활용하기 위해서, import에 JwtModule 설정을 진행.

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -5,9 +5,22 @@ import { DbModule } from '../database/db.module'; // 👈 이거 추가
 import { LoginService } from './login.service';
 import { IdCheckService } from './idCheck.service';
 import {EmailCheckService} from "./emailCheck.service";
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 
 @Module({
-    imports: [DbModule],
+    imports: [
+        DbModule, 
+        JwtModule.registerAsync({
+            imports: [ConfigModule], // 👈 ConfigModule 주입
+            inject: [ConfigService],
+        useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_TOKEN_SECRET'),
+        signOptions: { expiresIn: '1h' },
+      }),
+    }),
+],
     controllers: [AuthController],
     providers: [AuthService, LoginService, IdCheckService, EmailCheckService],
 })
