26dcdb3501299fe2e03b40ae871ff2a4eb874a54
김민지 <twoji3019@naver.com>
2025-05-27 12:47:26 +0900

    Fix : 비밀번호 확인 먼저 입력시 일치여부 확인

--- client/src/components/signup/fields/PasswordField.tsx
+++ client/src/components/signup/fields/PasswordField.tsx
@@ -27,6 +27,14 @@ export function PasswordField({value, onChange, onValidChange}: InputProps) {
     else {
       setPwError('')
     }
+
+        //비밀번호 확인 값이 있으면 일치 여부 
+    if(pwCheck.length>0 && pwCheck===pw){
+      setCheckError('일치합니다')
+    } else {
+      setCheckError('비밀번호가 일치하지 않습니다')
+    }
+
     onValidChange?.(isValid);
     onChange?.(e);
   }
