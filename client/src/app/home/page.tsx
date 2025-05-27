f359be590b4e3d5d365e6575a7f79221266bc6cc
최현준 <choijjunii1@gmail.com>
2025-05-27 15:43:09 +0900

    Feat: Home Section 컴포넌트 생성

--- client/src/app/home/page.tsx
+++ client/src/app/home/page.tsx
@@ -1,9 +1,12 @@
+import { StockSection } from "@/components/home/StockSection"
+import { NewsSection } from "@/components/home/NewsSection";
+
 export default function Home() {
 
   return (
     <div>
       <div>
-      <h1>홈페이지</h1>
+        <h1>홈페이지</h1>
       </div>
 
       <div>
