5cb35f971bcfd08093316e55ef099fddb49dff5c
김민지 <twoji3019@naver.com>
2025-05-28 12:21:31 +0900

    Feat : Modal창에 확인 버튼 클릭 시 이벤트 props로 전달

--- client/src/components/home/StockSection.tsx
+++ client/src/components/home/StockSection.tsx
@@ -13,14 +13,23 @@ export function StockSection() {
   //모달창
   const [showModal, setShowModal] = useState(false)
 
+  //모달에서 확인 버튼 클릭시
+  const handleConfirm=()=>{
+    setHasAccount(true)
+    setShowModal(false)
+  }
+
+  //!차트, 계좌번호, 계좌 임시 
+  const account : number = 1;
+
   return (
     <>
       <Section
         title="주식"
         // description="주식 시장의 최신 동향과 정보를 확인하세요."
-        children={hasAccount ? <p>내 계좌</p> : <CreateStock onClick={()=>setShowModal(true)}/>}
+        children={hasAccount ? <p>내 계좌 {account}</p> : <CreateStock onClick={()=>setShowModal(true)}/>}
       />
-      {showModal && <Modal type='create'/>}
+      {showModal && <Modal type='create' onConfirm={handleConfirm}/>}
     </>
   );
 }
