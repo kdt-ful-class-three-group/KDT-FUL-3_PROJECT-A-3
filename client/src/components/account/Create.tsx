b06a5d11ef232b04380b1e228140614819ad56b9
김민지 <twoji3019@naver.com>
2025-05-28 10:50:59 +0900

    Feat : Modal에 내보내기 위한 틀 작성

---
+++ client/src/components/account/Create.tsx
@@ -0,0 +1,9 @@
+// 계좌 생성
+
+export default function Create(){
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ client/src/components/account/Trade.tsx
@@ -0,0 +1,9 @@
+// 주식 거래
+
+export default function Trade(){
+  return(
+    <div>
+      
+    </div>
+  )
+}
