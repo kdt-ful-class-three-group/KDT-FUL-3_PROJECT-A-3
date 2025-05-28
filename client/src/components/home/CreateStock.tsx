8fcfdc2cdbea876c0dfd48f260bbe42a952edd13
김민지 <twoji3019@naver.com>
2025-05-28 12:02:34 +0900

    Feat : 전달받은 props 적용

--- client/src/components/home/CreateStock.tsx
+++ client/src/components/home/CreateStock.tsx
@@ -1,10 +1,14 @@
 // 계좌 생성 버튼
 import { Button } from "../common/Button"
 
-export default function CreateStock(){
+type props = {
+  onClick:()=>void
+}
+
+export default function CreateStock({onClick}:props){
 
   return(
-    <div style={{display:'flex'}}>
+    <div style={{display:'flex', cursor:'pointer'}} onClick={onClick}>
       <Button variant="icon" icon="plus"/>
       <p>계좌 생성</p>
     </div>
