1898dcb7e82ea076db43a46bdd9ab820cb7e1fb9
전선일 <jeonsunil1225@gamil.com>
2025-05-28 12:38:17 +0900

    Feat: 어세스 토큰의 존재 유무에 따른 홈페이지 랜더링 변화 로직으로 수정

--- client/src/app/home/page.tsx
+++ client/src/app/home/page.tsx
@@ -1,9 +1,51 @@
+'use client';
+
+import axios from "axios";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
 export default function Home() {
+  const [authStatus, setAuthStatus] = useState<'loading' | 'ok' | 'no'>('loading');
 
-  return (
-    <div>
-      <h1>홈페이지</h1>
+  useEffect(() => {
+    axios.post('http://localhost:8008/auth/refresh', {}, {
+      withCredentials: true, // 쿠키 전송
+    })
+    .then(res => {
+      console.log('refresh 성공', res.data);
+      setAuthStatus('ok');
+    })
+    .catch(err => {
+      console.log('refresh 실패', err.data);
+      setTimeout(() => {
+      setAuthStatus('no');
+      }, 50);
+    });
+  }, []);
 
-    </div>
-  )
+  // ✅ 아직 판단 중
+  if (authStatus === 'loading') {
+    return <div>로딩 중...</div>;
+  }
+
+  // ✅ 인증 성공
+  if (authStatus === 'ok') {
+    return (
+      <div>
+        <h1>홈페이지</h1>
+      </div>
+    );
+  }
+
+    if (authStatus === 'no') {
+    return (
+      <div>
+        <h1>로그인이 필요한 페이지 입니다.</h1>
+      <Link href='/login'>로그인 페이지로 이동</Link>
+      </div>
+    );
+  }
+
+  // ✅ 인증 실패한 경우는 리다이렉트가 걸려 있어서 렌더링 안 해도 됨
+  return null;
 }
