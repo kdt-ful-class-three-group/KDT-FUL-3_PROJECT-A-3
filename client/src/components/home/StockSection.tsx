64c412a024560875218ad2036c05463ff374206d
김민지 <twoji3019@naver.com>
2025-05-28 11:37:11 +0900

    Feat : 계좌 생성 버튼 추가

--- client/src/components/home/StockSection.tsx
+++ client/src/components/home/StockSection.tsx
@@ -1,11 +1,17 @@
+import { Button } from "../common/Button";
 import { Section } from "../common/Section";
 
 export function StockSection() {
   return (
     <Section
       title="주식"
-      description="주식 시장의 최신 동향과 정보를 확인하세요."
-      children={<p>주식 섹션 내용이 여기에 들어갑니다.</p>}
+      // description="주식 시장의 최신 동향과 정보를 확인하세요."
+      children={
+      <div>
+        <Button variant="icon" icon="plus"/>
+        <p>계좌 생성</p>
+      </div>
+    }
     />
   );
 }
