71ae29f94b886979fac942800afce35b942d0b30
김민지 <twoji3019@naver.com>
2025-05-28 18:09:27 +0900

    Feat : 차트 생성 중

--- client/src/components/home/MyStock.tsx
+++ client/src/components/home/MyStock.tsx
@@ -11,16 +11,54 @@ import {
   Legend
 } from 'chart.js'
 import { useMemo } from "react"
-
+//차트 요소 등록
+ChartJS.register(LineElement, CategoryScale,LinearScale, PointElement, Tooltip, Legend)
 
 export default function MyStock({account, money}:{account:string, money:string}){
 
   //!차트데이터 (임시)
   // date, money (asset)
   const moneyData = [
-    {date:'2025-05-28',asset:money} // 계좌 생성 직후
+    {date:'2025-05-28',asset:Number(money)} // 계좌 생성 직후
   ]
   
+  //차트용 데이터
+  const data = useMemo(()=>({
+    labels: moneyData.map(item=>item.date),
+    datasets:[
+      {
+        label:'자산',
+        data:moneyData.map(item=>item.asset),
+        borderColor:'#ccc',
+        backgroundColor:'rgba(229,229,229,0.2)',
+        fill : true,
+        tension: 0.3,
+        pointRadius:3,
+      }
+    ]
+  }),[moneyData])
+
+  //차트옵션
+  const options = {
+    responsive:true,
+    plugins:{
+      legend:{
+        display:false,
+      },
+      tooltip:{
+        callbacks:{
+          label:(ctx:any)=>`${ctx.parsed.y.toLocaleString()}`
+        }
+      }
+    },
+    scales:{
+      y:{
+        ticks:{
+          callback:(value:number)=>`${Number(value).toLocaleString()}`
+        }
+      }
+    }
+  }
 
   return (
     <div>
@@ -30,7 +68,8 @@ export default function MyStock({account, money}:{account:string, money:string})
       </div>
       <div>
         <p>{money}</p>
-        <Button name='>' type='button'/>
+        {/* 마이페이지로 이동 */}
+        <Button name='>' type='button'/> 
       </div>
       <div>
         <p>금액 변동률</p>
