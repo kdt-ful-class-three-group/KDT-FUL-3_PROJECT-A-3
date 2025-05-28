b754f590fefcfdce681ad786e79a1ba9daa5ebf0
김민지 <twoji3019@naver.com>
2025-05-28 11:56:53 +0900

    Feat : 계좌 유무에 따른 상태값 설정

--- client/src/components/home/StockSection.tsx
+++ client/src/components/home/StockSection.tsx
@@ -1,17 +1,23 @@
-import { Button } from "../common/Button";
+'use client'
+import { useState } from "react";
 import { Section } from "../common/Section";
+//계좌 생성 버튼
+import CreateStock from "./CreateStock";
+import { Modal } from "../account/Modal";
 
 export function StockSection() {
+
+  //신규 회원인지, 계좌 유무에 따른 상태값 설정
+  const [hasAccount, setHasAccount] = useState(false)
+
+  //모달창
+  const [showModal, setShowModal] = useState(false)
+
   return (
     <Section
       title="주식"
       // description="주식 시장의 최신 동향과 정보를 확인하세요."
-      children={
-      <div>
-        <Button variant="icon" icon="plus"/>
-        <p>계좌 생성</p>
-      </div>
-    }
+      children={hasAccount ? <p>내 계좌</p> : <CreateStock />}
     />
   );
 }
