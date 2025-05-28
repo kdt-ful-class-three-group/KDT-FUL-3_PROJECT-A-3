a95c2acecc445f217f30f42470efd34fc6297dd7
김민지 <twoji3019@naver.com>
2025-05-28 15:05:08 +0900

    Feat : 주식데이터로 증감 계산해봄

--- client/src/components/common/menu/StatusBar.tsx
+++ client/src/components/common/menu/StatusBar.tsx
@@ -10,19 +10,30 @@ type iconProps = {
 }
 
 // 주식 - aapl, tsla, nvda
-// 주식 - 증감
+// 주식 - 증감 close 데이터 사용
 // 변동값 % = ((현재가 - 전일 종가)/전일종가) * 100
 
 export default function StatusBar(){
   //온도
   const [temp, setTemp]= useState(0)
+  //주식
+  const [stock, setStock]= useState('')
 
   //한강물
   useEffect(()=>{
     async function fetchData(){
       try{
+        //수온데이터
         const waterRes = await axios.get('https://api.ivl.is/hangangtemp/')
         setTemp(waterRes.data.temperature)
+
+        //주식 데이터 최신 2개 비교
+        const stockRes = await axios.get('http://localhost:8008/stocks/tsla')
+        const current = parseFloat(stockRes.data.values[0].close);
+        const prev = parseFloat(stockRes.data.values[1].close);
+
+        const change = ((current-prev)/prev)*100
+        setStock(change.toFixed(2)+'%')
       }
       catch(err){
         console.log('수온 데이터 가져오기 실패',err)
@@ -33,6 +44,6 @@ export default function StatusBar(){
   },[])
 
   return(
-    <div></div>
+    <div>{temp} {stock}</div>
   )
 }
