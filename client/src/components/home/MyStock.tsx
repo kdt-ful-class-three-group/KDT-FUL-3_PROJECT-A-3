f23617191222320971b0b8e5ef1ee9b6843632e2
김민지 <twoji3019@naver.com>
2025-05-28 17:58:49 +0900

    Feat : chartjs 사용하기 위해 모듈 가져옴

--- client/src/components/home/MyStock.tsx
+++ client/src/components/home/MyStock.tsx
@@ -1,9 +1,26 @@
 import { Button } from "../common/Button"
+//차트
+import { Line } from "react-chartjs-2"
+import {
+  Chart as ChartJS,
+  LineElement,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  Tooltip,
+  Legend
+} from 'chart.js'
+import { useMemo } from "react"
+
 
 export default function MyStock({account, money}:{account:string, money:string}){
 
-  //차트데이터
+  //!차트데이터 (임시)
   // date, money (asset)
+  const moneyData = [
+    {date:'2025-05-28',asset:money} // 계좌 생성 직후
+  ]
+  
 
   return (
     <div>
@@ -19,7 +36,7 @@ export default function MyStock({account, money}:{account:string, money:string})
         <p>금액 변동률</p>
       </div>
       <div>
-        <p>차트</p>
+        <Line data={data} options={options}/>
       </div>
     </div>
   )
