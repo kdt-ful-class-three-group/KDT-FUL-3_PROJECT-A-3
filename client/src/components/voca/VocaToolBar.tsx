092578dbfb5e9b9376ca59b8487c21d0fa0b197e
김민지 <twoji3019@naver.com>
2025-05-29 15:43:44 +0900

    Feat : 컴포넌트에 props 작성

--- client/src/components/voca/VocaToolBar.tsx
+++ client/src/components/voca/VocaToolBar.tsx
@@ -2,21 +2,37 @@
 
 import SearchVoca from "./SearchVoca"
 import Consonant from "./Consonant"
+//타입 지정
+type Item = {
+  //검색에 해당하는 데이터
+  voca:string;
+  name:string;
+  description:string;
+  formula:string|null
+}
 
 //타입 지정
 type Props<T> = {
   value:string;
   onChange:(value:string)=>void;
-  data:T[];
-  onSelect:(item:T)=>void;
+  data:Item[];
+  onSelect:(item:Item)=>void;
   onScroll : (initial:string)=>void;
 }
 
-export default function VocaToolBar(){
+export default function VocaToolBar<T extends {voca?:string; name:string;}>({value, onChange, data, onSelect, onScroll}:Props<T>){
 
   return(
     <div>
-      
+      <SearchVoca
+        value={value}
+        onChange={onChange}
+        data={data}
+        onSelect={onSelect}
+      />
+      <Consonant
+        onSelect={onScroll}
+      />
     </div>
   )
 }
