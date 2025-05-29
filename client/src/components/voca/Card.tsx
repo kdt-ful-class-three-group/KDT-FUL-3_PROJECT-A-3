6b748e8b77cf2eb5e7af8013d3ee8d57170f5ad5
김민지 <twoji3019@naver.com>
2025-05-29 11:38:03 +0900

    Test : 단어에 대한 임시 데이터 생성

---
+++ client/src/components/voca/Card.tsx
@@ -0,0 +1,22 @@
+// !임시 데이터
+const vocaList = [
+    {
+    voca : "PER",
+    name: "Price Earnings Ratio",
+    description: "주가를 주당순이익(EPS)으로 나눈 값으로, 주가가 기업의 이익에 비해 고평가 또는 저평가되었는지를 판단하는 지표입니다.",
+    formula: "PER = 주가 / EPS"
+  },
+  {
+    voca : "Limit Order",
+    name: "Limit Order",
+    description: "투자자가 지정한 가격 이상 또는 이하에서만 체결되도록 하는 주문입니다.",
+    formula: null
+  },
+]
+
+export default function Card(){
+
+  return(
+    <div></div>
+  )
+}
