7a8efc6e7da183392d419eb8eca7009299bd6af8
김민지 <twoji3019@naver.com>
2025-05-29 12:55:49 +0900

    Feat : 검색창에 값이 있으면 내용 보이기

--- client/src/components/voca/SearchVoca.tsx
+++ client/src/components/voca/SearchVoca.tsx
@@ -1,5 +1,5 @@
 // 검색 컴포넌트
-
+import { useState } from "react";
 import { Input } from "../common/Input";
 import { Button } from "../common/Button";
 
@@ -8,6 +8,8 @@ type Item = {
   //검색에 해당하는 데이터
   voca:string;
   name:string;
+  description:string;
+  formula:string|null
 }
 type Props = { //props로 가져올 데이터
   value:string; 
@@ -19,12 +21,18 @@ type Props = { //props로 가져올 데이터
 export default function SearchVoca({value, onChange, data, onSelect}:Props){
 
   //검색할 내용 상태값
+  const [search, setSearch] = useState<Item[]>([])
 
 
   return(
-    <div style={{display:'flex'}}>
-      <Input name='voca' type="search"/>
-      <Button name='검색'/>
+    <div style={{position:'relative'}}>
+      <div style={{display:'flex'}}>
+        <Input name='voca' type="search" value={value} onChange={(e)=>onChange(e.target.value)}/>
+        <Button name='검색'/>
+      </div>
+      <div style={{position:'absolute', backgroundColor:'#fff', width:'100%'}}>
+        {value && <p>검색중</p>}
+      </div>
     </div>
   )
 }
