56b3bb1a0135edf7d3511914c18cd3693fa2a602
김민지 <twoji3019@naver.com>
2025-05-29 10:15:42 +0900

    Feat : 차트 선 아래 색상 적용

--- client/src/components/home/MyStock.tsx
+++ client/src/components/home/MyStock.tsx
@@ -8,11 +8,12 @@ import {
   LinearScale,
   PointElement,
   Tooltip,
-  Legend
+  Legend,
+  Filler
 } from 'chart.js'
 import { useMemo } from "react"
 //차트 요소 등록
-ChartJS.register(LineElement, CategoryScale,LinearScale, PointElement, Tooltip, Legend)
+ChartJS.register(LineElement, CategoryScale,LinearScale, PointElement, Tooltip, Legend, Filler)
 
 export default function MyStock({account, money}:{account:string, money:string}){
 
@@ -20,7 +21,7 @@ export default function MyStock({account, money}:{account:string, money:string})
   // date, money (asset)
   const moneyData = [
     {date:'계좌만든 날',asset:1000000}, // 계좌 생성 직후
-    {date:'오늘',asset:1000000}, // 계좌 생성 직후
+    {date:'오늘',asset:1000000}, // 오늘
   ]
   
   //차트용 데이터
@@ -30,11 +31,12 @@ export default function MyStock({account, money}:{account:string, money:string})
       {
         label:'자산',
         data:moneyData.map(item=>item.asset),
-        borderColor:'#ccc',
-        backgroundColor:'rgba(229,229,229,0.2)',
+        borderColor:'#000',
+        backgroundColor:'rgba(0,0,0,0.6)',
         fill : true,
         tension: 0.3,
         pointRadius:3,
+        
       }
     ]
   }),[moneyData])
@@ -56,11 +58,16 @@ export default function MyStock({account, money}:{account:string, money:string})
       x:{
         // x축 범위 설정
         min:0, //시작은 첫 데이터
-        max: Math.max(4,moneyData.length-1)//최소 5칸 확보
+        max: Math.max(4,moneyData.length-1),//최소 5칸 확보
       },
       y:{
         ticks:{
-          callback:(value:string|number)=> Number(value).toLocaleString()
+          callback:(value:string|number)=> value===1000000 ? '1,000,000':''
+        },
+        min:0,
+        max:1200000,
+        grid:{
+          display:false //격자 안보이게 처리
         }
       }
     }
