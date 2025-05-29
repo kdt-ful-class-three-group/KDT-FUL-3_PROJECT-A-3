7b2a78cdbf148d1cd717940f14047cda7ccb8037
김민지 <twoji3019@naver.com>
2025-05-29 11:52:25 +0900

    Feat : 클릭 이벤트 추가

--- client/src/components/voca/Card.tsx
+++ client/src/components/voca/Card.tsx
@@ -1,3 +1,6 @@
+'use client'
+import { useState } from "react";
+
 // !임시 데이터
 const vocaList = [
     {
@@ -14,9 +17,27 @@ const vocaList = [
   },
 ]
 
-export default function Card(){
+type props = {
+  vocaObj:{
+    voca:string;
+    name:string;
+    description:string;
+    formula:string|null
+  }
+}
+
+export default function Card({vocaObj}:props){
+
+  const [click, isClick] = useState(false)
 
   return(
-    <div></div>
+    <div onClick={()=>isClick(!click)}>
+      <h1>{vocaObj?.voca}</h1>
+      <p>{vocaObj?.name}</p>
+      <div style={click ? {display:'block'}: {display:'none'}}>
+        <p>{vocaObj?.description}</p>
+        <p>{vocaObj?.formula}</p>
+      </div>
+    </div>
   )
 }
