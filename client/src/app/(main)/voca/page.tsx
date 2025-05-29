45eb86637eeeccd82898d9682fcf703e187d8f29
김민지 <twoji3019@naver.com>
2025-05-29 14:16:02 +0900

    Test : 한글 단어 추가

--- client/src/app/(main)/voca/page.tsx
+++ client/src/app/(main)/voca/page.tsx
@@ -7,17 +7,23 @@ import SearchVoca from "@/components/voca/SearchVoca";
 // !임시 데이터
 const vocaList = [
   {
-  voca : "PER",
-  name: "Price Earnings Ratio",
-  description: "주가를 주당순이익(EPS)으로 나눈 값으로, 주가가 기업의 이익에 비해 고평가 또는 저평가되었는지를 판단하는 지표입니다.",
-  formula: "PER = 주가 / EPS"
-},
-{
-  voca : "Limit Order",
-  name: "Limit Order",
-  description: "투자자가 지정한 가격 이상 또는 이하에서만 체결되도록 하는 주문입니다.",
-  formula: null
-},
+    voca : "PER",
+    name: "Price Earnings Ratio",
+    description: "주가를 주당순이익(EPS)으로 나눈 값으로, 주가가 기업의 이익에 비해 고평가 또는 저평가되었는지를 판단하는 지표입니다.",
+    formula: "PER = 주가 / EPS"
+  },
+  {
+    voca : "Limit Order",
+    name: "Limit Order",
+    description: "투자자가 지정한 가격 이상 또는 이하에서만 체결되도록 하는 주문입니다.",
+    formula: null
+  },
+  {
+    voca: "시장가 주문",
+    name: "Market Order",
+    description: "시장에 나온 가격으로 즉시 체결되는 주문 방식입니다.",
+    formula: null,
+  },
 ]
 
 export default function Voca() {
