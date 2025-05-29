f4a24eddc6a07bca2932a4d52252766976228a0f
최현준 <choijjunii1@gmail.com>
2025-05-29 10:26:48 +0900

    Fix : stockList 나이키,마이크로소프트 제거

--- client/src/components/stocks/StockList.tsx
+++ client/src/components/stocks/StockList.tsx
@@ -3,12 +3,10 @@ export const stockList = [
   { name: "구글" , symbol: "GOOGL"},
   { name: "엔비디아" , symbol: "NVDA"},
   { name: "애플" , symbol: "AAPL"},
-  { name: "마이크로소프트" , symbol: "MSFT"},
   { name: "아마존" , symbol: "AMZN"},
   { name: "넷플릭스" , symbol: "NFLX"},
   { name: "메타", symbol: "META" },
   { name: "스타벅스", symbol: "SBUX" },
-  { name: "나이키", symbol: "NKE" },
 ]
 
 export function getKoreanSymbol(symbol: string): string {
