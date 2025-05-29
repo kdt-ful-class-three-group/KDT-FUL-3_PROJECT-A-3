214191f8179b7792c254d2f8ec647fa57d28a0c3
Jadenmeister <bigdragon00@naver.com>
2025-05-30 00:56:04 +0900

    Feature: + 버튼에 계좌 생성 확인 로직 구성 예정

--- client/src/components/home/CreateStock.tsx
+++ client/src/components/home/CreateStock.tsx
@@ -7,8 +7,13 @@ type props = {
 
 export default function CreateStock({onClick}:props){
 
+    const createAccount = () => {
+        confirm("계좌를 생성하시겠습니까?");
+
+    }
+
   return(
-    <div style={{display:'flex', cursor:'pointer'}} onClick={onClick}>
+    <div style={{display:'flex', cursor:'pointer'}} onClick={createAccount}>
       <Button variant="icon" icon="plus"/>
       <p>계좌 생성</p>
     </div>
