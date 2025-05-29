8ee5557bff6e913a0803599e51e94da69ab2603f
김민지 <twoji3019@naver.com>
2025-05-29 11:53:17 +0900

    Test : 카드 컴포넌트 추가

--- client/src/app/(main)/voca/page.tsx
+++ client/src/app/(main)/voca/page.tsx
@@ -1,3 +1,5 @@
+import Card from "@/components/voca/Card";
+
 export default function Voca() {
   
   return (
@@ -5,6 +7,7 @@ export default function Voca() {
       <h1>사전페이지</h1>
       <p>여기 사전내용이 들어갑니다.</p>
       {/* 여기에 단어장 컴포넌트나 기능을 추가할 수 있습니다. */}
+      <Card vocaObj={{voca:'a',name:'b',description:'c',formula:'d'}}/>
     </div>
   );
 }
