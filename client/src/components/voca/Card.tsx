44466f0db63b4b7bf9444b0e5c09f52f105ff841
김민지 <twoji3019@naver.com>
2025-05-29 14:31:24 +0900

    Chore : 검색을 위한 id 속성 추가

--- client/src/components/voca/Card.tsx
+++ client/src/components/voca/Card.tsx
@@ -15,7 +15,8 @@ export default function Card({vocaObj}:props){
   const [click, isClick] = useState(false)
 
   return(
-    <div onClick={()=>isClick(!click)}>
+    // 검색 했을 때 스크롤 이동을 위한 id 추가 - 공백 없어야함 > replace 사용
+    <div onClick={()=>isClick(!click)} id={`card-${vocaObj.voca.replace(/\s/g,'')}`}>
       <h1>{vocaObj?.voca}</h1>
       <p>{vocaObj?.name}</p>
       <div style={click ? {display:'block'}: {display:'none'}}>
