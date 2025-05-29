c732225c47f7aa25f8c3e9943306558c746d82ea
김민지 <twoji3019@naver.com>
2025-05-29 12:28:02 +0900

    Test : 추가한 임시데이터로 card 컴포넌트 사용해봄

--- client/src/app/(main)/voca/page.tsx
+++ client/src/app/(main)/voca/page.tsx
@@ -1,13 +1,40 @@
+'use client'
+
 import Card from "@/components/voca/Card";
+import { useEffect, useState } from "react";
+
+// !임시 데이터
+const vocaList = [
+  {
+  voca : "PER",
+  name: "Price Earnings Ratio",
+  description: "주가를 주당순이익(EPS)으로 나눈 값으로, 주가가 기업의 이익에 비해 고평가 또는 저평가되었는지를 판단하는 지표입니다.",
+  formula: "PER = 주가 / EPS"
+},
+{
+  voca : "Limit Order",
+  name: "Limit Order",
+  description: "투자자가 지정한 가격 이상 또는 이하에서만 체결되도록 하는 주문입니다.",
+  formula: null
+},
+]
 
 export default function Voca() {
+
+  const [list, setList]=useState<typeof vocaList>([])
   
+  useEffect(()=>{
+    setList(vocaList)
+  },[])
+
   return (
     <div>
       <h1>사전페이지</h1>
       <p>여기 사전내용이 들어갑니다.</p>
       {/* 여기에 단어장 컴포넌트나 기능을 추가할 수 있습니다. */}
-      <Card vocaObj={{voca:'a',name:'b',description:'c',formula:'d'}}/>
+      {list.map((item, index)=>(
+        <Card key={index} vocaObj={item}/>
+      ))}
     </div>
   );
 }
