bb1b17d924bd3631f5dc51dbb50fb1e827d152d3
전선일 <jeonsunil1225@gamil.com>
2025-05-30 16:46:50 +0900

    Feat: 리덕스 툴킷을 사용해서 account_number와 asset 관리 및, 포스트 요청 엔드포인트 변경

--- client/src/components/account/Create.tsx
+++ client/src/components/account/Create.tsx
@@ -2,8 +2,9 @@
 
 import { Button } from "../common/Button";
 import axios from "axios";
-import {useState} from "react";
-
+import { useSelector } from "react-redux";
+import { RootState } from "@/store";
+ 
 
 type props = {
   //확인 버튼 클릭 
@@ -15,6 +16,7 @@ type props = {
  * @param onClose - 계좌 생성 완료 후 호출되는 콜백 함수 (선택)
  */
 export default function Create({onClose}: props) {
+  const {account_number, asset} = useSelector((state:RootState) => state.account)
 
 
 
@@ -24,14 +26,12 @@ export default function Create({onClose}: props) {
    * 계좌 생성 후 onClose 콜백을 호출하여 모달을 닫음
    */
    function creatAccount() {
-    axios.post('http://localhost:8008/account/create', {}, {
+    axios.post('http://localhost:8008/account/insertData', {account_number: account_number, asset: asset}, {
       withCredentials: true,
     }).then(res => {
       console.log(res.data);
       // setAccountNumber(res.data.accountNumber); // 서버에서 받은 계좌 번호 설정
-         const newA = res.data.account_number;
       if (onClose) onClose(); // 모달 닫기
-    alert(`계좌가 생성되었습니다. 계좌 번호: ${newA}`);
     });
 
   }
@@ -43,11 +43,11 @@ export default function Create({onClose}: props) {
       <p>확인 버튼을 누르면 완료됩니다</p>
       <div>
         <p>계좌 번호</p>
-        <p>{}</p>
+        <p>{account_number}</p>
       </div>
       <div>
         <p>초기 금액</p>
-        <p>1,000,000풀</p>
+        <p>{asset}풀</p>
       </div>
       {/* 버튼 */}
       <Button name='확인' type='button' onClick={creatAccount}/>
