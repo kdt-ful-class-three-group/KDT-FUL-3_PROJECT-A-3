24667aca31e8a6648a331f8aae4f14c0635d7a5a
JadenMeister <bigdragon00@naver.com>
2025-05-30 17:40:59 +0900

    Test: Stock endpoint 수정

--- server/src/stocks/stocks.controller.ts
+++ server/src/stocks/stocks.controller.ts
@@ -1,12 +1,23 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { StocksService } from './stocks.service';
+import {DbService} from "../database/db.service";
 
 @Controller ('stocks')
-export class StocksController{
-  constructor(private readonly stocksService: StocksService) {}
+export class StocksController {
+  constructor(private readonly stocksService: StocksService, private readonly db: DbService) {}
+
+
+  // @Get(':symbol')
+  // getStock(@Param('symbol') symbol: string) {
+  //   return this.stocksService.getStocksData(symbol);
+  // }
+  //
+  //
+
 
   @Get(':symbol')
   getStock(@Param('symbol') symbol: string) {
-    return this.stocksService.getStocksData(symbol);
+    return this.stocksService.getStockData(symbol);
   }
+
 }
