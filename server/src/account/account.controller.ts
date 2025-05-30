56d460d654ab9bd3c4667300970f6d6f6ca62271
김민지 <twoji3019@naver.com>
2025-05-30 11:39:31 +0900

    Feat : 로그인 후 jwt에서 정보 가져오기

--- server/src/account/account.controller.ts
+++ server/src/account/account.controller.ts
@@ -1,15 +1,19 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Get, Post, UseGuards , Req } from "@nestjs/common";
 import { AccountService } from "./account.service";
 import { AccountDto } from "./dto/account.dto";
 import { CheckService } from './check.service';
+import { GetService } from "./get.service";
+import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
 
 
 @Controller('account')
 export class AccountController {
   constructor(
     private readonly AccountService: AccountService,
-    private readonly CheckService: CheckService
+    private readonly CheckService: CheckService,
+    private readonly GetService: GetService
   ) {}
+
   @Get()
   async getAllAccount() {
     return this.CheckService.getAllAccount();
@@ -18,5 +22,13 @@ export class AccountController {
   @Post('make')
   async account(@Body() dto : AccountDto) {
     return this.AccountService.account(dto);
+  }
+
+  // 토큰 사용해서 계좌 정보 읽기
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  async getMyAccount(@Req() req){
+    // req.user.user_id 에서 유저 정보 추출
+    return this.GetService.getAccountByUserId(req.user.user_id)
   }
 }
---
+++ server/src/account/get.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { DbService } from '../database/db.service';
+
+
+@Injectable()
+export class GetService {
+  constructor(private readonly db: DbService) {}
+
+
+    async getAccountByUserId(userId:string) {
+    const result = await this.db.query(`
+      SELECT * FROM account WHERE user_id=$1
+      `,[userId]);
+      return result.rows[0];
+  }
+}
