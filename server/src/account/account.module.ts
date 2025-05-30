01ae2a6dcfcbecd0776fae9f62c1bc89859fa74b
김민지 <twoji3019@naver.com>
2025-05-30 12:13:12 +0900

    Feat : 추가한 파일에 대해 사용을 위한 providers 값 추가

--- server/src/account/account.module.ts
+++ server/src/account/account.module.ts
@@ -3,11 +3,12 @@ import { DbModule } from "src/database/db.module";
 import { AccountController } from "./account.controller";
 import { AccountService } from "./account.service";
 import { CheckService } from "./check.service";
+import { GetService } from "./get.service";
 
 
 @Module({
     imports: [DbModule],
     controllers: [AccountController],
-    providers: [AccountService, CheckService],
+    providers: [AccountService, CheckService, GetService],
 })
 export class AccountModule {}
