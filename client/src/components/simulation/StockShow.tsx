04bdccef19f239e9449186eea9ebadc891190cc3
김민지 <twoji3019@naver.com>
2025-05-30 16:13:01 +0900

    Feat : 버튼 클릭에 따른 출력 내용 변경 적용

--- client/src/components/simulation/StockShow.tsx
+++ client/src/components/simulation/StockShow.tsx
@@ -11,19 +11,33 @@ export default function StockShows  ({btnValue}:{btnValue:string}){
   // 관심
   const [interest, setInterest] = useState<string[]>([])
   //실시간
-  const [all, setAll]= useState<typeof stockList>(stockList)
+  const [all, setAll]= useState<string[]>(stockList.map(i=>i.symbol))
   //보여줄 데이터 선정
   const [take, setTake]= useState<string[]>([])
 
-  useEffect(()=>{
-    // !임시로 선정
-    setGet(['aapl'])>
-    setInterest(['tsla','googl'])
 
-  },[btnValue])
+  useEffect(()=>{
+
+    // !임시로 선정
+    setGet(['aapl'])
+    setInterest(['tsla','googl'])
+    
+  },[])
+  
+  useEffect(()=>{
+// btnValue에 따른 값 설정
+if(btnValue==='보유') setTake(get)
+if(btnValue==='관심') setTake(interest)
+if(btnValue==='실시간 순위') setTake(all)
+
+  },[btnValue,get,interest])
 
 
   return(
-    <div></div>
+    <div>
+      {take.map((item, i)=>(
+        <div key={i}>{item}</div>
+      ))}
+    </div>
   )
 }
