46bc5ec6ca25d83d71879cba192cac884c71dc70
최현준 <choijjunii1@gmail.com>
2025-05-30 11:27:23 +0900

    Feat : stock page 판매/구매 버튼,페이지 생성

---
+++ client/src/app/trade/page.tsx
@@ -0,0 +1,8 @@
+export default function TradePage() {
+  
+  return (
+    <div>
+      <p>판매/구매 페이지</p>
+    </div>
+  )
+}
--- client/src/components/stocks/TradeButton.tsx
+++ client/src/components/stocks/TradeButton.tsx
@@ -1,18 +1,25 @@
+'use client'
+import { useRouter } from "next/navigation"
 import { Button } from "../common/Button"
 
 export function TradeButton() {
-  
+  const router = useRouter()
+
+  const trade = () => {
+    console.log('판매/구매 페이지이동')
+    router.push('/trade')
+  }
+
   return (
     <div>
       <Button
         name="판매"
-        onClick={() => { }}
+        onClick={trade}
       />
       <Button
         name="구매"
-        onClick={() => { }}
+        onClick={trade}
       />
     </div>
-
   )
 }
