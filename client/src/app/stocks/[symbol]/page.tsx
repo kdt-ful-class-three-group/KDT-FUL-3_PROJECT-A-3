037450fbceffc9f8717368763b45b95d0cb2ab98
JadenMeister <bigdragon00@naver.com>
2025-06-02 12:46:48 +0900

    Fix: twelveData -> db 자체 데이터로 변경

--- client/src/app/stocks/[symbol]/page.tsx
+++ client/src/app/stocks/[symbol]/page.tsx
@@ -1,8 +1,10 @@
 'use client'
 
 import { useState, useEffect } from "react"
+import { useRouter } from "next/navigation"
 import { useParams } from "next/navigation"
 import { StockChart } from "@/components/stocks/StockChart"
+import { Button } from "@/components/common/Button"
 import { StockInfo } from "@/components/stocks/StockInfo"
 import { StockData } from "@/types/stock"
 import { StockTradeButton } from "@/components/stocks/StockTradeButton"
@@ -10,36 +12,35 @@ import { StockTradeButton } from "@/components/stocks/StockTradeButton"
 import axios from "axios"
 
 export default function StockPage() {
-  const { symbol } = useParams() as { symbol: string };
-  const [stockData, setStockData] = useState<StockData | null>(null);
+  const params = useParams();
+  const symbol = (params?.symbol ?? '') as string;
+  const [stockData, setStockData] = useState<StockData  | null>(null);
+  const router = useRouter();
 
   useEffect(() => {
     if (!symbol) return;
-
-    const fetchStockData = async () => {
-      try {
-        const res = await axios.get<StockData>(`http://localhost:8008/stocks/${symbol}`);
-        setStockData(res.data);
-      } catch (error) {
-        console.error('주식 데이터를 불러오는 데 실패했습니다:', error);
-      }
-    };
-
-    fetchStockData();
-  }, [symbol]);
+    axios.get<StockData>(`http://localhost:8008/stocks/${symbol}`)
+      .then(res => setStockData(res.data))
+      .catch(console.error);
+  }, [symbol])
 
   if (!stockData) return <div>주식불러오는중...</div>;
 
   return (
     <div>
       <div>
+        <Button
+          name="뒤로가기"
+          onClick={() => router.back()}
+        />
+      </div>
+      <div>
         <StockInfo
-          symbol={stockData?.meta?.symbol ?? ''}
-          price={stockData?.values?.[0]?.close}
+            symbol={stockData.symbol ?? 'UNKNOWN'}
+            price={stockData.close}
         />
-        <StockChart
-          stockData={stockData?.values || []}
-        />
+        <StockChart data={stockData.values} symbol={stockData.symbol} />
+
       </div>
       <div>
         {stockData.values[0]?.close !== undefined && (
