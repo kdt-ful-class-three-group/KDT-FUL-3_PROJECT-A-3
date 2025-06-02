7bd1023ba1390f0eeeadaad9d9a8ff4648bccaff
김민지 <twoji3019@naver.com>
2025-06-02 15:20:19 +0900

    Test: 더미데이터로 도넛차트 생성

--- client/src/app/portfolio/page.tsx
+++ client/src/app/portfolio/page.tsx
@@ -1,10 +1,12 @@
+'use client'
 import DoughnutChart from "@/components/portfolio/Doughnut"
+import { tradeItems } from "@/components/portfolio/mocks/tradeItem"
 
 export default function portfolioPage(){
   return(
     <div>
       포트폴리오
-      <DoughnutChart />
+      <DoughnutChart trades={tradeItems}/>
     </div>
   )
 }
