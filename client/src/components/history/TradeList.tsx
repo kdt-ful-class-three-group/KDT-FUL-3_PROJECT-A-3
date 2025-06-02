c18865d82006ccbe32b250cb873ef3f214601904
김민지 <twoji3019@naver.com>
2025-06-02 18:07:27 +0900

    Chore : 필요한 상태값 작성

--- client/src/components/history/TradeList.tsx
+++ client/src/components/history/TradeList.tsx
@@ -1,8 +1,30 @@
 import { tradeItems } from "../portfolio/mocks/tradeItem";
 import TradeFilter from "./TradeFilter";
 import TradeTable from "./TradeTable";
+import { useState } from "react";
+import { stockList } from "../stocks/StockList";
+// 타입
+import { TradeItem, TradeType } from "@/types/tradeItem";
+type Props={
+  trades:TradeItem[]
+}
 
-export default function TradeList(){
+export default function TradeList({trades}:Props){
+
+  // 상태
+  // 시작일
+  const [startDate, setStartDate]=useState('')
+  // 종료일
+  const [endDate, setEndDate]=useState('')
+  // 종목
+  const [stock, setStock]=useState('all')
+  // 구매, 판매 타입
+  const [type, setType]=useState('all')
+
+  // 주식리스트
+  const stocks = stockList.map(i=> i.symbol)
+
+
   return(
     <div>
       <TradeFilter />
