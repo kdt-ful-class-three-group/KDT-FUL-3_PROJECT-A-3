2267f1ed26eca13979bb8e23ed6a24715a559d1c
전선일 <jeonsunil1225@gamil.com>
2025-06-02 17:16:21 +0900

    Feat: 앱, 어카운트 모듈에 추가한 서비스들 추가

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -7,9 +7,10 @@ import {StocksModule} from "./stocks/stocks.module";
 import { JwtModule } from '@nestjs/jwt';
 import { AccountModule } from './account/account.module';
 import { VocaModule } from './voca/voca.module';
+import { UserPortfolioModule } from './userportfolio/userportfolio.module';
 
 @Module({
 
-    imports: [ConfigModule.forRoot({isGlobal:true}),AuthModule, UsersModule, StocksModule, JwtModule, AccountModule,VocaModule],
+    imports: [ConfigModule.forRoot({isGlobal:true}),AuthModule, UsersModule, StocksModule, JwtModule, AccountModule,VocaModule, UserPortfolioModule],
 })
 export class AppModule {}
