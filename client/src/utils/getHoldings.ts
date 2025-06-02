5361dcb8124bb80ee1c42b2104cf7ae5e5aacf70
김민지 <twoji3019@naver.com>
2025-06-02 16:10:00 +0900

    Chore : 오타 수정

---
+++ client/src/utils/getHoldings.ts
@@ -0,0 +1,74 @@
+// 거래 기록을 기반으로 종목별 보유 현황 구하기
+
+// !더미데이터 - 거래 기록 , 현재가
+import { currentPrices } from "@/components/portfolio/mocks/currentPrices";
+import { AmpContext } from "next/dist/server/route-modules/pages/vendored/contexts/entrypoints";
+
+// 타입
+enum TradType {
+  Buy = 0,
+  Sell = 1
+}
+
+type tradeItem = {
+  id:number; //계좌순서
+  account_number: number; //계좌
+  user_name:string; //유저
+  stock_name:string//종목명
+  price:number; //거래 당식 종목 가격
+  much: number; //거래량
+  buy_sale : TradType; //구매 또는 판매
+  date: string; //거래일자
+}
+
+export type HoldingItem={
+  stock_name:string;
+  quantity:number; //매수 총량 - 매도 총량
+  avgPrice : number ; //평균 매수 단가
+  currentPrice: number //현재 주가 - 정의한 가격 사용
+}
+
+export function getHoidingWithAvg(items:tradeItem[]):HoldingItem[]{
+
+  // 종목별 집계용 Map : 보유 수량, 총 매수 수량, 총 매수 금액
+  const map = new Map<
+  string,
+  {quantity: number; totalBuy: number; buyAmount: number}
+  >()
+
+  for(const item of items){
+    const record = map.get(item.stock_name) || {
+      quantity: 0,
+      totalBuy: 0,
+      buyAmount: 0
+    }
+
+    // 매수
+    if(item.buy_sale===1){
+      record.quantity += item.much;
+      record.totalBuy += item.much;
+      record.buyAmount += item.much * item.price
+    }
+    // 매도
+    else {
+      record.quantity -= item.much
+    }
+
+    map.set(item.stock_name, record)
+  }
+
+  const result: HoldingItem[]=[]
+
+  for(const [stock_name, data] of map.entries()){
+    // 보유수량 >0 인 종목만 결과에 포함
+    if(data.quantity>0){
+      result.push({
+        stock_name,
+        quantity: data.quantity,
+        avgPrice: Math.round(data.buyAmount / data.totalBuy), // 총 매입금액 / 총 매수 수량
+        currentPrice: currentPrices[stock_name] || 0 // 현재가 객체에서 조회 
+      })
+    }
+  }
+  return result
+}
