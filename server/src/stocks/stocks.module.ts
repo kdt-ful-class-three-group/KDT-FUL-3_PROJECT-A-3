4e93bdbbb6fa332dc89918472b6a140475c21cd4
JadenMeister <bigdragon00@naver.com>
2025-06-02 12:45:09 +0900

    Feat: db모듈, 서비스 추가

--- server/src/stocks/stocks.module.ts
+++ server/src/stocks/stocks.module.ts
@@ -1,8 +1,10 @@
 import {Module} from "@nestjs/common";
 import {StocksController} from "./stocks.controller";
 import {StocksService} from "./stocks.service";
+import {DbModule} from"../database/db.module";
 
 @Module({
+    imports: [DbModule],
     controllers: [StocksController],
     providers: [StocksService],
 })
