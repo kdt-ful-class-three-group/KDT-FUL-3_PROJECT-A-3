da865a6faaa8062023541481c393522175834d41
김민지 <twoji3019@naver.com>
2025-06-04 10:01:46 +0900

    Feat : 더미데이터로 select 구성

--- client/src/app/history/page.tsx
+++ client/src/app/history/page.tsx
@@ -2,6 +2,8 @@
 import TradeList from "@/components/history/TradeList"
 import { Button } from "@/components/common/Button"
 import { useRouter } from "next/navigation"
+// !임시데이터
+import { tradeItems } from "@/components/portfolio/mocks/tradeItem"
 
 export default function historyPage(){
   const router= useRouter()
@@ -10,7 +12,7 @@ export default function historyPage(){
     <div>
       <h1>거래 내역</h1>
       <Button name='뒤로가기' onClick={()=>router.back()}/>
-      <TradeList />
+      <TradeList trades={tradeItems} />
     </div>
   )
 }
--- client/src/components/history/TradeFilter.tsx
+++ client/src/components/history/TradeFilter.tsx
@@ -3,10 +3,28 @@ import { Input } from "../common/Input"
 import { Select } from "../common/Select"
 import { Button } from "../common/Button"
 
-export default function TradeFilter(){
+// 전달받은 props 타입 설정
+type Props = {
+  stock : string[];
+  selectedStock : string;
+  selectedType : string;
+  startDate:string;
+  endDate:string;
+  setSelectedStock : (val:string)=>void
+  setSelectedType: (val:string)=>void
+  setStartDate : (val:string)=>void
+  setEndDate : (val:string)=>void
+}
 
-  // 상태
-  // value
+export default function TradeFilter({
+  stock, selectedStock, selectedType, startDate, endDate, setSelectedStock, setSelectedType, setStartDate, setEndDate
+}:Props){
+
+  // 전체 추가
+  const stockOption = [{name:'전체', value:'all'}]
+  stock.forEach(i=>{
+    stockOption.push({name:i, value:i})
+  })
 
   return(
     <div style={{display:'flex'}}>
@@ -19,11 +37,11 @@ export default function TradeFilter(){
       </div>
       {/* 종목 */}
       <div>
-        <Select option={[{name:'',value:''}]}></Select> 
+        <Select option={stockOption}></Select> 
       </div>
       {/* 거래 */}
       <div>
-        <Select option={[{name:'',value:''}]}></Select>
+        <Select option={[{name:'전체',value:'all'},{name:'구매',value:'buy'},{name:'판매',value:'sell'}]}></Select>
       </div>
     </div>
   )
