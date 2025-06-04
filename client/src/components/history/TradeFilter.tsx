bf0f728cceabd821e6a500a122f87ebce7b2ca1a
김민지 <twoji3019@naver.com>
2025-06-04 09:51:36 +0900

    Feat : filter로 적용할 props 작성

--- client/src/components/history/TradeFilter.tsx
+++ client/src/components/history/TradeFilter.tsx
@@ -1,14 +1,19 @@
 // 타입
 import { Input } from "../common/Input"
 import { Select } from "../common/Select"
+import { Button } from "../common/Button"
 
 export default function TradeFilter(){
 
+  // 상태
+  // value
+
   return(
-    <div>
+    <div style={{display:'flex'}}>
       {/* 전체 선택 */}
+      <Button name="전체" />
       {/* 날짜 */}
-      <div>
+      <div style={{display:'flex'}}>
         <Input type="date" label="시작"/>
         <Input type="date" label="종료"/>
       </div>
--- client/src/components/history/TradeList.tsx
+++ client/src/components/history/TradeList.tsx
@@ -9,7 +9,7 @@ type Props={
   trades:TradeItem[]
 }
 
-export default function TradeList({trades}:Props){
+export default function TradeList({trades=[]}:Props){ //undefined일 때 에러발생 -> 초기값 설정
 
   // 상태
   // 시작일
@@ -17,17 +17,46 @@ export default function TradeList({trades}:Props){
   // 종료일
   const [endDate, setEndDate]=useState('')
   // 종목
-  const [stock, setStock]=useState('all')
+  const [selectedStock, setSelectedStock]=useState('all')
   // 구매, 판매 타입
-  const [type, setType]=useState('all')
+  const [selectedType, setSelectedType]=useState('all')
 
   // 주식리스트
-  const stocks = stockList.map(i=> i.symbol)
+  const stockNames = trades.map(i=> i.stock_name)
+  const uniqueStock = Array.from(new Set(stockNames)) //중복 제외
+
+  // 필터 적용시키기
+  const filtered = trades.filter(item=>{
+    const date = item.date.split('T')[0] //날짜
+    const type = item.buy_sale===TradeType.Buy ? '구매' : '판매' //구매,판매
+
+    // 시작날짜
+    const afterStart = !startDate || date >= startDate
+    // 종료날짜
+    const beforeEnd = !endDate || date<=endDate
+    // 주식종목
+    const matchStock = selectedStock ==='all' || selectedStock===item.stock_name
+    // 타입
+    const matchType = selectedType === 'all' || selectedType === type
+
+    // 반환
+    return afterStart && beforeEnd && matchStock && matchType
+  })
 
 
   return(
     <div>
-      <TradeFilter />
+      <TradeFilter 
+        stock = {uniqueStock}
+        selectedStock = {selectedStock}
+        selectedType = {selectedType}
+        setSelectedStock = {setSelectedStock}
+        setSelectedType = {setSelectedType}
+        startDate = {startDate}
+        endDate = {endDate}
+        setStartDate = {setStartDate}
+        setEndDate = {setEndDate}
+      />
       <TradeTable trades={tradeItems}/>
     </div>
   )
