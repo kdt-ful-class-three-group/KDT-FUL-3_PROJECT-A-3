055c21ad001bf203f55387b78966dd8f4e47f3c0
김민지 <twoji3019@naver.com>
2025-06-05 12:11:39 +0900

    Test : 더미데이터 변경하는 테스트 중

--- client/src/app/history/page.tsx
+++ client/src/app/history/page.tsx
@@ -4,9 +4,30 @@ import { Button } from "@/components/common/Button"
 import { useRouter } from "next/navigation"
 // !임시데이터
 import { tradeItems } from "@/components/portfolio/mocks/tradeItem"
+import { useEffect, useState } from "react"
+import axios from "axios"
 
 export default function historyPage(){
   const router= useRouter()
+
+  // 데이터
+  const [tradeData, setTradeData] = useState([])
+
+  useEffect(()=>{
+    const fetchData = async()=>{
+      try{
+        const res = await axios.get('http://localhost:8008/')
+      }
+      catch(err){
+        console.log('거래 내역 가져오기 실패',err)
+      }
+
+    }
+
+    // 실행
+    fetchData()
+
+  },[tradeData])
 
   return(
     <div>
