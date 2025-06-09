6b204d2cd69ed478ea3485c7cb0fb0313cc3a34b
JadenMeister <bigdragon00@naver.com>
2025-06-09 15:46:37 +0900

    fix: module.css , css -> tailwind 변경 작업 및 tradeContent에서 심볼 -> 아이콘으로 변경

--- client/src/components/trade/TradeContent.tsx
+++ client/src/components/trade/TradeContent.tsx
@@ -1,9 +1,10 @@
 import { useSelector } from 'react-redux';
 import { RootState } from '@/store';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { TradeContentProps } from "@/types/trade";
 import { TradeKeypad } from "./TradeKeypad";
 import styles from './TradeStyles.module.css'
+import {iconMap} from '../stocks/StockIconMaps';
 
 export function TradeContent({ mode }: TradeContentProps) {
   const { symbol, price } = useSelector((state: RootState) => state.trade)
@@ -17,23 +18,27 @@ export function TradeContent({ mode }: TradeContentProps) {
   }
 
   return (
-    <div>
-      <div className={styles.amountBox}>
-        <h1 className={styles.symbol}>{symbol}</h1>
-        <div className={styles.subHeader}>
-          <span>시장가: {price}풀</span>
-        </div>
-      </div>
       <div>
-        <TradeKeypad
-          symbol={symbol}
-          amount={amount}
-          setAmount={setAmount}
-          mode={mode}
-          price={price}
-        />
-      </div>
+        <div className="bg-[#f1f3f5] justify-center items-center rounded-xl p-4 mx-auto mb-5 max-w-[320px]">
+          <h1 className="text-[30px] font-bold text-center mb-1">
+    <span className="flex items-center justify-center gap-2">
+      {(iconMap[symbol] || (() => <span>{symbol}</span>))()} {symbol}
+    </span>
+          </h1>
+          <div className="flex font-bold w-full max-w-[300px] text-sm mx-auto justify-center">
+            <span>시장가: {price}풀</span>
+          </div>
+        </div>
+        <div>
+          <TradeKeypad
+              symbol={symbol}
+              amount={amount}
+              setAmount={setAmount}
+              mode={mode}
+              price={price}
+          />
+        </div>
 
-    </div>
+      </div>
   )
 }
