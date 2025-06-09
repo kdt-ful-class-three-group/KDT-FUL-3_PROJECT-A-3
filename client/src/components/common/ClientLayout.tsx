c5af0ea877a8c0610f2a9088592c5415e7895b8b
최현준 <choijjunii1@gmail.com>
2025-06-09 16:27:52 +0900

    Fix : ClientLayout 컴포넌트 생성

---
+++ client/src/components/common/ClientLayout.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+import { Nav } from "@/components/common/menu/Nav";
+
+export default function ClientLayout({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+  const hideNav = pathname.startsWith('/login') || pathname.startsWith('/signup') || pathname.startsWith('/find');
+
+  return (
+    <>
+      {!hideNav && <Nav />}
+      {children}
+    </>
+  );
+}
--- client/src/components/history/TradeTable.tsx
+++ client/src/components/history/TradeTable.tsx
@@ -1,24 +1,40 @@
 // 거래내역 리스트
 //타입
 import { TradeItem } from "@/types/tradeItem"
+import { iconMap } from "../stocks/StockIconMaps"
 
 export default function TradeTable({trades}:{trades:TradeItem[]}){
   return (
     <div className="divide-y text-sm">
       {trades.map((item) => (
-        <div key={item.id} className="py-3 px-2">
-          <div className="flex justify-between items-center font-medium">
-            <div className="text-gray-800">{item.symbol}</div>
-            <div className={item.buy_sell ? "text-blue-600" : "text-red-600"}>
-              {item.buy_sell ? "+" : "-"}
-              {(item.price * item.much).toLocaleString()} 풀
+        <div
+          key={item.id}
+          className="flex items-center justify-between w-full px-4 py-3 border-b last:border-none"
+        >
+          <div className="flex items-center gap-3">
+            <div className="w-10 h-10 bg-white border border-gray-300 rounded-full flex items-center justify-center text-xl">
+              {iconMap[item.symbol] ? iconMap[item.symbol]() : item.symbol[0]}
+            </div>
+            <div>
+              <h4 className="text-sm font-semibold text-gray-900">{item.symbol}</h4>
+              <p className="text-xs text-gray-500">{item.much.toLocaleString()}주</p>
             </div>
           </div>
-          <div className="text-xs text-gray-500 mt-1 flex justify-between">
-            <div>{item.date.split("T")[0]}</div>
-            <div className="text-gray-400">
-              {item.buy_sell ? "구매" : "판매"} | {item.much}주 · {item.price.toLocaleString()}풀
-            </div>
+          <div className="text-right">
+            <p className="text-sm font-bold text-gray-800">
+              {(item.price * item.much).toLocaleString()}풀
+            </p>
+            <p
+              className={`text-xs font-medium ${
+                item.buy_sell ? 'text-red-500' : 'text-blue-600'
+              }`}
+            >
+              {item.buy_sell ? '+' : '-'}
+              {item.price.toLocaleString()}
+            </p>
+            <p className="text-xs text-black">
+              {item.date.split("T")[0]}
+            </p>
           </div>
         </div>
       ))}
