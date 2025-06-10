6cfc1d3453b2dd54b01e987b19709daa26db5d27
JadenMeister <bigdragon00@naver.com>
2025-06-10 11:57:24 +0900

    chore: intro.js id값 설정

--- client/src/app/(main)/simulation/page.tsx
+++ client/src/app/(main)/simulation/page.tsx
@@ -2,11 +2,20 @@
 
 import AccountInfo from "@/components/simulation/AccountInfo";
 import StockTab from "@/components/simulation/StockTab";
+import {Button} from "@/components/common/Button";
+import {useEffect} from "react";
+import introJs from "intro.js";
+import 'intro.js/introjs.css';
+import {startIntro} from "@/app/(main)/utils/startIntro";
 
 export default function Simulation() {
 
+
   return (
-    <div className="max-w-xl w-full mx-auto px-4 mt-[5%]">
+    <div id="simlation" className="max-w-xl w-full mx-auto px-4 mt-[5%]">
+        <div className="flex justify-end mb-3">
+        <Button name="가이드라인" className="w-37 h-10 bg-[#B0DB9C] rounded-lg text-white cursor-pointer" onClick={()=>{startIntro()}} />
+        </div>
       <AccountInfo />
       <StockTab />
     </div>
