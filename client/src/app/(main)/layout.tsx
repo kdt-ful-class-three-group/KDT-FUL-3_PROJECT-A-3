1909206cec51ac6ac155ed0e0b15bd8825fd4261
김민지 <twoji3019@naver.com>
2025-06-10 15:37:18 +0900

    Fix : nav의  레이아웃 스타일 변경

--- client/src/app/(main)/layout.tsx
+++ client/src/app/(main)/layout.tsx
@@ -1,12 +1,12 @@
 // app/(main)/layout.tsx
 import { Header } from "@/components/common/menu/Header";
-import startIntro from "./utils/startIntro";
+// import startIntro from "./utils/startIntro";
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
 
 
     return (
-    <div id="simulation" className="max-w-xl w-full mx-auto px-4 mt-[5%]">
+    <div id="simulation" className="max-w-xl w-full mx-auto px-4 mt-[2%]">
       <Header />
       {children}
     </div>
